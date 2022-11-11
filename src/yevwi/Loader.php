fa70d2bbc76d4c5481412bf060995df35376466e
nayzu <80849223+nayzu@users.noreply.github.com>
2022-11-11 20:58:43 +0100

    Rewrite some things

--- src/yevwi/Loader.php
+++ src/yevwi/Loader.php
@@ -9,30 +9,41 @@ use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
-class Loader extends PluginBase implements Listener
-{
-    public function onEnable(): void
-    {
-        $this->getLogger()->info("Size command Enabled!");
+class Loader extends PluginBase implements Listener {
+
+    /**
+     * @return void
+     */
+    public function onEnable(): void {
+        $this->getLogger()->info("§aSizeCmd initialized");
     }
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
-    {
+    /**
+     * @param CommandSender $sender
+     * @param Command $command
+     * @param string $label
+     * @param array $args
+     * @return bool
+     */
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         switch ($command->getName()) {
             case "size":
                 if (!$sender instanceof Player) {
                     return false;
                 }
                 if (!is_numeric($args[0])) {
-                    $sender->sendMessage(TextFormat::colorize('&cDebes ingresar un valor numerico!'));
+                    $sender->sendMessage("&cToo small size");
                     return false;
                 }
                 if (count($args) < 0) {
-                    $sender->sendMessage(TextFormat::colorize('&cUsa /size [int: scale]'));
+                    $sender->sendMessage("&cUse /size [size]");
                     return false;
                 }
                 if (!$sender->hasPermission("size.command")) {
                     return false;
+                }
+                if (count($args) > 30) {
+                    $sender->sendMessage("§cToo large size");
                 }
                 $sender->setScale($args[0]);
         }
